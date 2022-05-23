78c8d92110bb644a8d740e693023965f66c85862
shuntaka9576 <shuntaka9576@gmail.com>
2022-05-23 20:48:54 +0900

    refactor(worker): refactor worker

--- restore.go
+++ restore.go
@@ -3,6 +3,7 @@ package ddbrew
 import (
 	"bufio"
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"os"
@@ -10,6 +11,9 @@ import (
 	"strings"
 	"sync/atomic"
 
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/pkg/errors"
 )
 
@@ -64,71 +68,142 @@ func Restore(ctx context.Context, opt *RestoreOption) error {
 		return nil
 	}
 
-	tasks := make(chan BatchTask)
-	results := make(chan BatchResult)
+	tasks := make(chan Task)
+	results := make(chan Result)
 
 	for i := 0; i < opt.Procs; i++ {
-		go batchWorker(i, tasks, results)
+		go worker(i, tasks, results)
 	}
 
 	reader := bufio.NewReader(f)
 
 	var remainedCount int64
+	readLine := 0
 	inputDone := make(chan struct{})
 
 	go func() {
-		var lines []string
+		notifyTask := func(wreq []types.WriteRequest) {
+			batchReq := map[string][]types.WriteRequest{}
+			batchReq[opt.TableName] = wreq
+			input := &dynamodb.BatchWriteItemInput{
+				RequestItems: batchReq,
+			}
 
-		notifyTask := func() {
-			tasks <- BatchTask{
+			tasks <- &RestoreTask{
 				tableName: opt.TableName,
-				jsonLines: lines,
+				req:       input,
 				ctx:       ctx,
 			}
 			atomic.AddInt64(&remainedCount, 1)
 		}
 
+		var wreqs []types.WriteRequest
+
 		for {
-			line, err := reader.ReadString('\n')
+			jl, err := reader.ReadString('\n')
 			if err != nil {
 				if err == io.EOF {
-					notifyTask()
+					notifyTask(wreqs)
+
 					break
 				}
 			}
-			lines = append(lines, strings.TrimSpace(line))
 
-			if len(lines) == perBatchRecordSize-1 || err == io.EOF {
-				notifyTask()
+			tjl := strings.TrimSpace(jl)
+			m := make(map[string]interface{})
+			err = json.Unmarshal([]byte(tjl), &m)
+			if err != nil {
+				fmt.Printf("json marshal err %s\n", err)
 
-				lines = nil
+				continue
 			}
+
+			attributeMap, err := attributevalue.MarshalMap(m)
+			if err != nil {
+				fmt.Printf("marshal error %s\n", err)
+
+				continue
+			}
+
+			wreqs = append(wreqs, types.WriteRequest{
+				PutRequest: &types.PutRequest{
+					Item: attributeMap,
+				},
+			})
+
+			if len(wreqs) == perBatchRecordSize-1 || err == io.EOF {
+				notifyTask(wreqs)
+
+				wreqs = nil
+			}
+			readLine += 1
 		}
 
 		close(inputDone)
 	}()
 
+	writed := 0
+	done := false
 	for {
 		select {
 		case result := <-results:
-			if result.Err != nil {
+			writed += result.Count()
+			if done {
+				fmt.Printf("\rreadLine:(done)%d, writeLine: %d", readLine, writed)
+			} else {
+				fmt.Printf("\rreadLine: %d, writeLine: %d", readLine, writed)
+			}
 
-				return errors.Wrap(result.Err, "batch write err")
+			if result.Error() != nil {
+				return errors.Wrap(result.Error(), "batch write err")
 			} else {
 				atomic.AddInt64(&remainedCount, -1)
 			}
-
-			if len(result.UnprocessedItems) > 0 {
-				for _, v := range result.UnprocessedItems {
-					fmt.Printf("%v\n", v)
-				}
-			}
 		case <-inputDone:
+			done = true
 			if remainedCount == 0 {
 				return nil
 			}
 		}
 	}
+}
+
+type RestoreResult struct {
+	count int
+	error error
+}
+
+func (t *RestoreResult) Error() error {
+	return t.error
+}
+
+func (t *RestoreResult) Count() int {
+	return t.count
+}
+
+type RestoreTask struct {
+	tableName string
+	req       *dynamodb.BatchWriteItemInput
+	ctx       context.Context
+}
+
+func (t *RestoreTask) Run() Result {
+	r, err := ddbClient.BatchWriteItem(t.ctx, t.req)
+	result := &RestoreResult{}
+
+	if err != nil {
+		result.error = err
+	}
+
+	if err == nil {
+		if r != nil && len(r.UnprocessedItems) > 0 {
+			result.count = len(t.req.RequestItems[t.tableName]) - len(r.UnprocessedItems)
+		} else {
+			result.count = len(t.req.RequestItems[t.tableName])
+		}
+	}
+
+	return result
 }
 
 func printProcess(size int, perBatchRecordSize int, jsonLineSize int, perBatchRecordSizeExp int, writeTimePerInterval int, procs int) {
--- worker.go
+++ worker.go
@@ -1,74 +1,17 @@
 package ddbrew
 
-import (
-	"context"
-	"encoding/json"
-	"fmt"
-
-	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
-)
-
-type BatchTask struct {
-	tableName string
-	jsonLines []string
-	ctx       context.Context
+type Task interface {
+	Run() Result
 }
 
-type BatchResult struct {
-	Value            int64
-	Task             BatchTask
-	Err              error
-	UnprocessedItems map[string][]types.WriteRequest
+type Result interface {
+	Count() int
+	Error() error
 }
 
-func batchWorker(id int, tasks <-chan BatchTask, results chan<- BatchResult) {
+func worker(id int, tasks <-chan Task, results chan<- Result) {
 	for t := range tasks {
-		batchReq := map[string][]types.WriteRequest{}
-		var reqs []types.WriteRequest
-
-		for _, jl := range t.jsonLines {
-			m := make(map[string]interface{})
-			err := json.Unmarshal([]byte(jl), &m)
-			if err != nil {
-				fmt.Printf("json marshal err %s\n", err)
-
-				continue // skip record
-			}
-
-			attributeMap, err := attributevalue.MarshalMap(m)
-			if err != nil {
-				fmt.Printf("marshal error %s\n", err)
-
-				continue // skip record
-			}
-
-			reqs = append(reqs, types.WriteRequest{
-				PutRequest: &types.PutRequest{
-					Item: attributeMap,
-				},
-			})
-		}
-
-		batchReq[t.tableName] = reqs
-
-		result := BatchResult{
-			Task: t,
-		}
-
-		input := &dynamodb.BatchWriteItemInput{
-			RequestItems: batchReq,
-		}
-
-		writeResult, err := ddbClient.BatchWriteItem(t.ctx, input)
-		if err != nil {
-			result.Err = err
-		}
-
-		if writeResult != nil && len(writeResult.UnprocessedItems) > 0 {
-			result.UnprocessedItems = writeResult.UnprocessedItems
-		}
+		result := t.Run()
 
 		results <- result
 	}
